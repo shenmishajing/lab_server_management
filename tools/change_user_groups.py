ac0b3cdd9e0809bc0a3039794f96f412a960e495
shenmishajing <shenmishajing@gmail.com>
2022-06-24 10:29:44 +0800

    fix wrong orde for user_name and user_group param of gpasswd cmd in change_user_groups.py

--- tools/change_user_groups.py
+++ tools/change_user_groups.py
@@ -3,7 +3,7 @@ from utils import do_something_on_users
 
 @do_something_on_users
 def change_user_groups(user_name, user_group = 'sudo', is_add = True, **kwargs):
-    return [f'gpasswd -{"a" if is_add else "d"} {user_group} {user_name}']
+    return [f'gpasswd -{"a" if is_add else "d"} {user_name} {user_group}']
 
 
 def main():
