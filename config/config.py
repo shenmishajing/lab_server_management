64deac92e5f7b1b3dafed6fcf2ed38f51133f28e
shenmishajing <shenmishajing@gmail.com>
2022-06-28 12:58:23 +0800

    add data_dir_path in config.yaml new logic for new_user.py

--- config/config.py
+++ config/config.py
@@ -1,16 +1,20 @@
 import yaml
 
-config = yaml.load(open('config/config.yaml'), Loader = yaml.Loader)
+server_config = yaml.load(open('config/config.yaml'), Loader = yaml.Loader)
 
-server_config = {} if 'server_config' not in config or config['server_config'] is None else config['server_config']
+server_config['compute_servers'] = list(sorted(server_config['compute_servers']))
+server_config['data_servers'] = list(sorted(server_config['data_servers']))
+server_config['servers'] = list(sorted(server_config['data_servers'] + server_config['compute_servers']))
 
-config['compute_servers'] = list(sorted(config['compute_servers']))
-config['data_servers'] = list(sorted(config['data_servers']))
-config['servers'] = list(sorted(config['data_servers'] + config['compute_servers']))
 for k in ['servers', 'compute_servers', 'data_servers']:
-    server_config[k] = [f'lab_{s}' for s in config[k]]
+    server_config[k] = [f'lab_{s}' for s in server_config[k]]
+
 for server in server_config['servers']:
     if server not in server_config:
         server_config[server] = {}
-    for k, v in config['default'].items():
+    for k, v in server_config['default'].items():
         server_config[server].setdefault(k, v)
+
+for k in list(server_config['data_dir_path'].keys()):
+    server_config['data_dir_path'][f'lab_{k}'] = server_config['data_dir_path'][k]
+    del server_config['data_dir_path'][k]
--- tools/new_user.py
+++ tools/new_user.py
@@ -8,8 +8,8 @@ def get_next_id(used_ids, start_id = 2300, end_id = 65535):
     return user_id
 
 
-def add_users(user_names, servers = None, admin_users = False, user_passwd = 'HangZhou2022', use_cache = False, user_id_start = 2300,
-              user_id_end = 65535):
+def add_users(user_names, servers = None, admin_users = False, user_passwd = 'HangZhou2022', request_change_password = True,
+              use_cache = False, user_id_start = 2300, user_id_end = 65535):
     users, already_used_ids, users_to_host = get_all_users(use_cache)
 
     if not isinstance(user_names, list):
@@ -37,19 +37,27 @@ def add_users(user_names, servers = None, admin_users = False, user_passwd = 'Ha
         cmd = []
         is_data_server = server in server_config['data_servers']
         for user_name, admin_user in zip(user_names, admin_users):
-            if user_name in users_to_host and server in users_to_host[user_name]:
-                continue
             user_id, group_id = user_ids[user_name]
-            cmd.extend([
-                           f'mkdir -p /data2/{user_name}',
-                           f'chown -R {user_id}:{group_id} /data2/{user_name}',
-                       ] if is_data_server else ([
-                                                     f'useradd {user_name} -s /bin/bash -d /home/{user_name} -m',
-                                                     f'echo {user_name}:{user_passwd} | chpasswd',
-                                                     f'usermod -u {user_id} {user_name}',
-                                                     f'groupmod -g {group_id} {user_name}',
-                                                     f'chage -d0 {user_name}'
-                                                 ] + ([f'usermod -G sudo {user_name}'] if admin_user else [])))
+
+            if (user_name not in users_to_host or server not in users_to_host[user_name]) and (admin_user or not is_data_server):
+                cmd.extend([
+                    f'useradd {user_name} -s /bin/bash -d /home/{user_name} -m',
+                    f'echo {user_name}:{user_passwd} | chpasswd',
+                    f'usermod -u {user_id} {user_name}',
+                    f'groupmod -g {group_id} {user_name}'
+                ])
+                if request_change_password:
+                    cmd += [f'chage -d0 {user_name}']
+
+            if is_data_server:
+                cmd.extend([
+                    f'mkdir -p /{server_config["data_dir_path"][server]}/{user_name}',
+                    f'chown -R {user_id}:{group_id} /{server_config["data_dir_path"][server]}/{user_name}'
+                ])
+
+            if admin_user:
+                cmd.append(f'gpasswd -a {user_name} sudo')
+
         run_ssh_cmd(cmd, server)
 
 
