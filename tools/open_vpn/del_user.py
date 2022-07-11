0e1e2a674a4da0fb635d3d4e97d26c20453fd622
shenmishajing <shenmishajing@gmail.com>
2022-07-11 10:27:32 +0800

    fix name bug in del_user.py

--- tools/open_vpn/del_user.py
+++ tools/open_vpn/del_user.py
@@ -1,17 +1,17 @@
 from utils import run_ssh_cmd
 
 
-def add_users(user_names):
+def del_users(user_names):
     if not isinstance(user_names, list):
         user_names = [user_names]
 
     server = f'lab_235'
-    cmd = [f'sh /data/openvpn_script/del_user.sh {" ".join(user_names)}']
+    cmd = [f'sh /data/openvpn_script/del_vpn_user.sh {" ".join(user_names)}']
     run_ssh_cmd(cmd, server)
 
 
 def main():
-    add_users('intern10')
+    del_users('intern10')
 
 
 if __name__ == '__main__':
