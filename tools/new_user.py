b7c7909d5669adb1528345d70b28de2dc2c22e9e
shenmishajing <shenmishajing@gmail.com>
2022-10-31 12:24:55 +0800

    remove data server in new_user.py

--- tools/new_user.py
+++ tools/new_user.py
@@ -35,11 +35,10 @@ def add_users(user_names, servers = None, admin_users = False, user_passwd = 'Ha
         if isinstance(server, int):
             server = f'192.168.0.{server}'
         cmd = []
-        is_data_server = server in server_config['data_servers']
         for user_name, admin_user in zip(user_names, admin_users):
             user_id, group_id = user_ids[user_name]
 
-            if (user_name not in users_to_host or server not in users_to_host[user_name]) and (admin_user or not is_data_server):
+            if user_name not in users_to_host or server not in users_to_host[user_name]:
                 cmd.extend([
                     f'useradd {user_name} -u {user_id} -s /bin/bash -d /data2/{user_name} -m',
                     f'groupmod -g {group_id} {user_name}',
@@ -47,12 +46,6 @@ def add_users(user_names, servers = None, admin_users = False, user_passwd = 'Ha
                 ])
                 if request_change_password:
                     cmd += [f'chage -d0 {user_name}']
-
-            if is_data_server:
-                cmd.extend([
-                    f'mkdir -p /{server_config["data_dir_path"][server]}/{user_name}',
-                    f'chown -R {user_id}:{group_id} /{server_config["data_dir_path"][server]}/{user_name}'
-                ])
 
             if admin_user:
                 cmd.append(f'gpasswd -a {user_name} sudo')
