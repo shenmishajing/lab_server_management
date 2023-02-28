7ec14c74a4515e5d9a4b4336d2420f5ee4fbdc0d
shenmishajing <shenmishajing@gmail.com>
2023-02-28 14:19:47 +0800

    use run del user script first to active ovpn scripts, add download zip feature

--- tools/open_vpn/ovpn_add_user.py
+++ tools/open_vpn/ovpn_add_user.py
@@ -1,6 +1,7 @@
-import os
 import argparse
-from utils import get_scp_clients, run_ssh_cmd, launcher
+import os
+
+from utils import get_scp_clients, launcher, run_ssh_cmd
 
 
 def parser_builder():
@@ -12,24 +13,34 @@ def parser_builder():
         nargs="+",
         help="user names, can be a list",
     )
+    parser.add_argument(
+        "--download-zip",
+        action="store_true",
+        help="whether to download the zip file, default not",
+    )
     return parser
 
 
 @launcher(parser_builder)
-def main(user_names):
+def main(user_names, download_zip):
     if not isinstance(user_names, list):
         user_names = [user_names]
 
     server = 235
-    cmd = [f'sh /data/openvpn_script/vpn_user.sh {" ".join(user_names)}']
+    cmd = [
+        f'sh /data/openvpn_script/del_vpn_user.sh {" ".join(user_names)}',
+        f'sh /data/openvpn_script/vpn_user.sh {" ".join(user_names)}',
+    ]
     run_ssh_cmd(cmd, server)
-    scp_client = get_scp_clients(hosts=server)
-    for user_name in user_names:
-        scp_client.get(
-            f"/data/openvpn_client/{user_name}.zip",
-            os.path.expanduser(f"~/Downloads/{user_name}.zip"),
-        )
-    scp_client.close()
+
+    if download_zip:
+        scp_client = get_scp_clients(hosts=server)
+        for user_name in user_names:
+            scp_client.get(
+                f"/data/openvpn_client/{user_name}.zip",
+                os.path.expanduser(f"~/Downloads/{user_name}.zip"),
+            )
+        scp_client.close()
 
 
 if __name__ == "__main__":
